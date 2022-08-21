5db783f1f067efb3fd444cbaf1f0a813965e9ae2
Thomas Gerdenitsch <ITN171298@qualifizierung.at>
2022-08-21 02:14:50 +0200

    blank

--- Gmoto/Gmoto/Controllers/ProductsController.cs
+++ Gmoto/Gmoto/Controllers/ProductsController.cs
@@ -45,117 +45,118 @@ namespace Gmoto.Controllers
             return View(product);
         }
 
-        //GET: Products/Create
-        public IActionResult Create()
-        {
-            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id");
-            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id");
-            return View();
-        }
+        ////GET: Products/Create
+        //public IActionResult Create()
+        //{
+        //    ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id");
+        //    ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id");
+        //    return View();
+        //}
 
         //POST: Products/Create
         //To protect from overposting attacks, enable the specific properties you want to bind to.
         //For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ProductName,NetUnitPrice,ImagePath,Sizes,Description,ManufacturerId,SubcategoryId")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id", product.ManufacturerId);
-            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", product.SubcategoryId);
-            return View(product);
-        }
 
-        //GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,ProductName,NetUnitPrice,ImagePath,Sizes,Description,ManufacturerId,SubcategoryId")] Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(product);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id", product.ManufacturerId);
+        //    ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", product.SubcategoryId);
+        //    return View(product);
+        //}
 
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id", product.ManufacturerId);
-            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", product.SubcategoryId);
-            return View(product);
-        }
+        ////GET: Products/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var product = await _context.Products.FindAsync(id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id", product.ManufacturerId);
+        //    ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", product.SubcategoryId);
+        //    return View(product);
+        //}
 
         //POST: Products/Edit/5
         //To protect from overposting attacks, enable the specific properties you want to bind to.
         //For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductName,NetUnitPrice,ImagePath,Sizes,Description,ManufacturerId,SubcategoryId")] Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,ProductName,NetUnitPrice,ImagePath,Sizes,Description,ManufacturerId,SubcategoryId")] Product product)
+        //{
+        //    if (id != product.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id", product.ManufacturerId);
-            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", product.SubcategoryId);
-            return View(product);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(product);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!ProductExists(product.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "Id", "Id", product.ManufacturerId);
+        //    ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", product.SubcategoryId);
+        //    return View(product);
+        //}
 
         //GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var product = await _context.Products
-                .Include(p => p.Manufacturer)
-                .Include(p => p.Subcategory)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
+        //    var product = await _context.Products
+        //        .Include(p => p.Manufacturer)
+        //        .Include(p => p.Subcategory)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(product);
-        }
+        //    return View(product);
+        //}
 
-        //POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var product = await _context.Products.FindAsync(id);
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        ////POST: Products/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var product = await _context.Products.FindAsync(id);
+        //    _context.Products.Remove(product);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool ProductExists(int id)
         {
--- Gmoto/Gmoto/Controllers/ShoppingCartController.cs
+++ Gmoto/Gmoto/Controllers/ShoppingCartController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gmoto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +10,63 @@ namespace Gmoto.Controllers
 {
     public class ShoppingCartController : Controller
     {
-        public IActionResult Index()
+        private readonly GMOTOContext _context;
+
+        public ShoppingCartController(GMOTOContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var gMOTOContext = _context.Products.Include(p => p.Manufacturer).Include(p => p.Subcategory);
+            return View(await gMOTOContext.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult AddProductToCart(int id)
+        {
+                HttpContext.Response.Cookies.Append("product_id", id.ToString());
+                var userId = HttpContext.Request.Cookies["product_id"];
+                var gMOTOContext = _context.Products.Include(p => p.Manufacturer).Include(p => p.Subcategory).ToList();
+                return View("Index", gMOTOContext);
+        }
+        //public IActionResult DeleteProductAtCart(int id)
+        //{
+
+        //}
+
+        // Bestellung
+        public IActionResult CreateEditOrder(Order order)
+        {
+            // TODO: write Order to DB (Order first dann OrderLine)
             return View();
         }
 
-        public IActionResult AddDeleteProduct(int id)
+        //Warenkorb
+        public IActionResult CreateEditOrderLine(OrderLine orderLine, Order order)
         {
+            // TODO: write OrderLine to DB (erst checken obs eine Order gibt)
             return View();
         }
+
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var product = await /*_context.*/Products
+        //        .Include(p => p.Manufacturer)
+        //        .Include(p => p.Subcategory)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(product);
+        //}
     }
 }
